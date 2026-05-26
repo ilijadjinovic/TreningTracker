c916524b96b6a67f043dad8c6d8f5493a8b3dbc4
ilijadjinovic <ilija.djinovic@gmail.com>
2026-05-26 14:35:08 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='tv5';const A=['/','/index.html'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/index.html'))))});
